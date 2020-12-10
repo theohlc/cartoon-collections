33a55beb95dbc96e77206bc2f846085675fd01ab
theo <teddyhoover@gmail.com>
2020-12-10 13:05:31 -0600

    commit tree 5513a9178eb88c7fe7f99bdd13f453ce50265362

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, i|
+    puts "#{i + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(summons)
+  summons.collect do |word|
+    word = word + "!"
+    word = word.capitalize
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  !calls.all? do |word|
+    word.length <= 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  first_cheese = nil
+
+  while first_cheese == nil
+    first_cheese = cheese_types[i] if cheeses.include?(cheese_types[i])
+    i += 1
+    puts first_cheese
+    break if i > cheeses.length 
+  end
+
+  first_cheese
 end
